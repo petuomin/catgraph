239477f940dfed48e8dc921061aafdddfd519583
Pasi Tuominen <pasi.e.tuominen@helsinki.fi>
2014-03-14 10:17:49 +0200

    changed graph->deps since there is no 'graph' option

--- graph.sh
+++ graph.sh
@@ -1,4 +1,4 @@
-node analyze.js --format=graph core/*.js modules/*.js >deps.dot
+node analyze.js --format=deps core/*.js modules/*.js >deps.dot
 node analyze.js --format=events core/*.js modules/*.js >events.dot
 dot deps.dot -Tpng -o deps.png
 dot events.dot -Tpng -o events.png
