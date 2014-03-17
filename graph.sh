8e77363122c09c1fa0811b7b957bb3c8ace6721e
Juho Vuori <juho.vuori@helsinki.fi>
2014-03-17 10:36:14 +0200

    validator graph

--- graph.sh
+++ graph.sh
@@ -1,11 +1,14 @@
 CORE="`find core/ -not -name Application.js -name \*.js`"
 MODULES="`find modules/ -name \*.js` json!modules/Toolbar_files/Toolbar_config.json"
+VALIDATORS="`find validators/ -name \*.js`"
 APP="core/Application.js"
 node analyze.js --format=deps ${CORE} ${MODULES} ${APP} >deps.dot
 node analyze.js --format=deps ${CORE} >deps-core.dot
 node analyze.js --core=sink --format=deps ${MODULES} >deps-modules.dot
+node analyze.js --core=sink --format=deps ${VALIDATORS} >deps-validators.dot
 node analyze.js --format=events ${CORE} ${MODULES} ${APP} >events.dot
 dot deps.dot -Tpng -o deps.png
 dot deps-core.dot -Tpng -o deps-core.png
 dot deps-modules.dot -Tpng -o deps-modules.png
+dot deps-validators.dot -Tpng -o deps-validators.png
 dot events.dot -Tpng -o events.png
